a378a68e539693bbcf67a92c258959065e159f4c
Jazz <yoyomonkey1000@hotmail.com>
2021-11-02 13:15:01 +0000

    started on caesar standard program

---
+++ Caesar-Cipher/caesar-standard/caesar-standard.py
@@ -0,0 +1,27 @@
+# caesar-standard is another attempt at doing the caesar cipher 
+# which uses a symbol set and a key
+# came from cracking codes with python
+
+original_message = "Hello again"
+decision = "d"
+output_message = ""
+SYMBOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+key = "2"
+
+def get_input():
+    return original_message
+
+
+def caesar_engine(textstream):
+
+
+
+def main():
+    print(get_input())
+
+
+
+
+
+
+main()
