8a5eac79855f15f2995af43302e8ee68dc81851c
Jazz <yoyomonkey1000@hotmail.com>
2021-11-02 03:27:46 +0000

    create caesar-basic

---
+++ Caesar-Cipher/caesar-basic.py
@@ -0,0 +1,14 @@
+# Caesar Cypher Basic Version from PCAP
+# This version only has a key of 1 and used the Ord and Chr string methods.
+# The Program will be improved overtime
+# By Jazz Arshad
+
+original_message = "Hello World"
+encrypted_message = ""
+
+original_message = original_message.lower()
+
+
+
+
+print(original_message, "will be encrypted to" , encrypted_message)
