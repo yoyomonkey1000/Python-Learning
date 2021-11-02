427fa85480e22a4c148f1923b8919cd03e9389d2
Jazz <yoyomonkey1000@hotmail.com>
2021-11-02 11:25:20 +0000

    created key1_encryption function

--- Caesar-Cipher/caesar-basic.py
+++ Caesar-Cipher/caesar-basic.py
@@ -3,20 +3,25 @@
 # The Program will be improved overtime
 # By Jazz Arshad
 
-original_message = "Hello World"
-encrypted_message = ""
-
-original_message = original_message.lower()
-
-for char in original_message:
-    if char == 'z':
-        encrypted_message += 'a'
-    elif char == ' ':
-        encrypted_message += ' '
-    else:
-        encrypted_message += (chr(ord(char)+1))
+orig_message = "Hello World"
+encr_message = ""
 
 
+def key1_encrypter(original_message): # this will shift the characters by 1 in the ascii chain
+
+    original_message = original_message.lower() #turn all chars into lowercase
+    encrypted_message= ""
+
+    for char in original_message: # for every char in the lower message
+        if char == 'z': # if its a z jus cycle it round to a
+            encrypted_message += 'a'
+        elif char == ' ': # if its a space leave it as a space
+            encrypted_message += ' '
+        else:  # if its any other character 
+            encrypted_message += (chr(ord(char)+1))
+    return encrypted_message
+    
 
 
-print(original_message, "will be encrypted to", encrypted_message)
+encr_message = key1_encrypter(orig_message)
+print(orig_message, "will be encrypted to", encr_message)
