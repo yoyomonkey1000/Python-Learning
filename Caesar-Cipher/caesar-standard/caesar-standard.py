b0d1fb0a1667a1d8e4bc0469dc65150f617f8eba
Jazz <yoyomonkey1000@hotmail.com>
2021-11-02 13:37:33 +0000

    created cypher_engine function

--- Caesar-Cipher/caesar-standard/caesar-standard.py
+++ Caesar-Cipher/caesar-standard/caesar-standard.py
@@ -2,7 +2,7 @@
 # which uses a symbol set and a key
 # came from cracking codes with python
 
-orig_message = "Hello again"
+input_message = "Hello again"
 decision = "d"
 output_message = ""
 SYMBOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -12,12 +12,15 @@ def get_input(original_message):
     return original_message
 
 
-def caesar_engine():
-    print("hello")
+def caesar_engine(original_message):
+    for char in original_message:
+        print(char)
+
 
 
 def main():
-    print(get_input())
+    print(get_input(input_message))
+    caesar_engine(input_message)
 
 
 
