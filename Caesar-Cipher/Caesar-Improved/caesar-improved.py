ff05df2fc2fc36204230463d2f22dcaf6494c054
Jazz <yoyomonkey1000@hotmail.com>
2021-11-04 23:06:15 +0000

    Encypted which keeps incase

--- Caesar-Cipher/Caesar-Improved/caesar-improved.py
+++ Caesar-Cipher/Caesar-Improved/caesar-improved.py
@@ -1,1 +1,26 @@
 # improved Caesar Cipher Exercise in the PCAP
+# Leeps letters case 
+# Do the ASCII ORD conversion + key 
+
+orig_message = "Hello World"
+out_message = ""
+key = 20
+char = ""
+
+for character in orig_message:
+    if character.isalpha():
+        char = ord(character)  + key
+    else:
+        out_message += character
+        continue
+    
+    if character.isupper():
+        char = ord(character)  + key
+        if char > ord('Z'):
+            char = char - 26
+    elif character.islower():
+        if char > ord('z'):
+            char = char -26
+    out_message += chr(char)
+
+print(out_message)
