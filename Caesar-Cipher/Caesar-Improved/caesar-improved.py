33476aa43be1645e995c73e78b9c18d1cff3e604
Jazz <yoyomonkey1000@hotmail.com>
2021-11-04 23:36:06 +0000

    created an encrypter/decrypter which preserves case in ascii

--- Caesar-Cipher/Caesar-Improved/caesar-improved.py
+++ Caesar-Cipher/Caesar-Improved/caesar-improved.py
@@ -4,23 +4,48 @@
 
 orig_message = "Hello World"
 out_message = ""
+choice = "e"
 key = 20
-char = ""
 
-for character in orig_message:
-    if character.isalpha():
-        char = ord(character)  + key
-    else:
-        out_message += character
-        continue
-    
-    if character.isupper():
-        char = ord(character)  + key
-        if char > ord('Z'):
-            char = char - 26
-    elif character.islower():
-        if char > ord('z'):
-            char = char -26
-    out_message += chr(char)
 
+
+def caesar_engine(original_message, choice):
+    output_message=""
+    char =""
+
+    if choice == 'e':
+        for character in original_message:
+            if character.isalpha():
+                char = ord(character)  + key
+            else:
+                output_message += character
+                continue 
+            if character.isupper():
+                if char > ord('Z'):
+                    char = char - 26
+            elif character.islower():
+                if char > ord('z'):
+                    char = char -26
+            output_message += chr(char)
+    elif choice == 'd':
+        for character in original_message:
+            if character.isalpha():
+                char = ord(character) - key
+            else:
+                output_message += character
+                continue
+            if character.isupper():
+                if char < ord('A'):
+                    char = char + 26
+            elif character.islower():
+                if char < ord('a'):
+                    char = char + 26
+            output_message += chr(char)
+    return output_message
+
+
+print(orig_message)
+out_message = (caesar_engine(orig_message, 'e'))
+print(out_message)
+out_message = (caesar_engine(out_message, 'd'))
 print(out_message)
