9de40f0291e3e8f01e69993eed9e5b04fd2608c6
Jazz <yoyomonkey1000@hotmail.com>
2021-11-05 03:08:13 +0000

    improved on Palindrome but not efficient

--- palindrome-detector/palindrome-detector.py
+++ palindrome-detector/palindrome-detector.py
@@ -3,12 +3,19 @@
 # turn to lowercase, checks is the first and 
 # last letter the same and works its way in. 
 
-inp_message = (input("Please enter in your string: "))
+import re
 
+#inp_message = (input("Please enter in your string: "))
+inp_message = "Go hang a salami, I’m a lasagna hog."
 
 def palindrome_engine(input_message):
     input_message = (input_message.lower()).replace(' ','') # strip out spaces from string
+    #input_message = (input_message.translate(str.maketrans('', '', string.punctuation()))) # from the import string package
+    input_message = re.sub(r'[^\w\s]', '', input_message)
+    print(input_message, " given to engine")
     
+
+
     i = 0 # set i to go up string to 0
     x = -1 # set x to -1 which is last character of a string
     print(int(len(input_message)/2)) # see if te string can be split properly
@@ -19,8 +26,8 @@ def palindrome_engine(input_message):
         else: 
             i += 1 # increment i to the next index in the string
             x -= 1 # decrement x to the prceeding index in the string
-
     return True
+
 
 if palindrome_engine(inp_message):
     print(f"The string {inp_message} is a palindrome")
