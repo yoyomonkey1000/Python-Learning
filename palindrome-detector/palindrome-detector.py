7555c0bae35fb7eb2660a1b8174ba4e12fc7b729
Jazz <yoyomonkey1000@hotmail.com>
2021-11-05 03:26:51 +0000

    updated palindrome: faster work with punctuation

--- palindrome-detector/palindrome-detector.py
+++ palindrome-detector/palindrome-detector.py
@@ -3,25 +3,29 @@
 # turn to lowercase, checks is the first and 
 # last letter the same and works its way in. 
 
-import re
-
-#inp_message = (input("Please enter in your string: "))
 inp_message = "Go hang a salami, I’m a lasagna hog."
+#inp_message = (input("Please enter in your string: "))
 
-def palindrome_engine(input_message):
+
+def palindrome_engine(input_message): 
+
     input_message = (input_message.lower()).replace(' ','') # strip out spaces from string
-    #input_message = (input_message.translate(str.maketrans('', '', string.punctuation()))) # from the import string package
-    input_message = re.sub(r'[^\w\s]', '', input_message)
-    print(input_message, " given to engine")
-    
-
-
+    preprocessed_message =""
     i = 0 # set i to go up string to 0
     x = -1 # set x to -1 which is last character of a string
-    print(int(len(input_message)/2)) # see if te string can be split properly
-    while i <= (int(len(input_message)/2)):
-        print(input_message[i] ," compared with " , input_message[x])
-        if (input_message[i]) != (input_message[x]): # if the strings dont match just return false
+     
+    # THIS DOESNT WORK input_message = (input_message.translate(str.maketrans('', '', string.punctuation()))) # from the import string package  
+    
+    for char in input_message: # go through every char in the string 
+        if  not char.isalnum(): # if the character is alpha numeric write it in
+            continue
+        else:
+            preprocessed_message += char 
+    print(preprocessed_message, " given to engine")
+    print(int(len(preprocessed_message)/2)) # see if te string can be split properly
+    while i <= (int(len(preprocessed_message)/2)):
+        print(preprocessed_message[i] ," compared with " , preprocessed_message[x])
+        if (preprocessed_message[i]) != (preprocessed_message[x]): # if the strings dont match just return false
             return False
         else: 
             i += 1 # increment i to the next index in the string
@@ -33,3 +37,4 @@ if palindrome_engine(inp_message):
     print(f"The string {inp_message} is a palindrome")
 else:
     print(f"The string {inp_message} is not a palidrome")
+
