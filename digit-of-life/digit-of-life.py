f4232bd7ab17ffd6ceffa82d6fb8bf1f96db65e4
Jazz <yoyomonkey1000@hotmail.com>
2021-11-06 19:37:38 +0000

    Digit of Life started but not working fully

---
+++ digit-of-life/digit-of-life.py
@@ -0,0 +1,17 @@
+#digit of life
+# get a string of numbers add them up
+# keep adding the digits until its a single digit
+
+number = "12345678"
+
+total = 0
+
+if True:
+    for char in number:
+        total += (int(char))
+        print(total)
+
+print("Final Total is ", total)
+
+
+
